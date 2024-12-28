13b4a3e4aac0240e32c81d5be13303de3a2e4dc3
Adam Kunzler <adamkunzler@gmail.com>
2024-12-28 10:36:04 -0600

    hash table and interned strings

--- cCraftingInterpretersCLox/CraftingInterpreters.CLox/compiler.c
+++ cCraftingInterpretersCLox/CraftingInterpreters.CLox/compiler.c
@@ -130,6 +130,8 @@ static void endCompiler() {
 }
 
 static void expression();
+static void statement();
+static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
@@ -258,6 +260,16 @@ static void expression() {
 	parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void declaration() {
+	statement();
+}
+
+static void statement() {
+	//if (match(TOKEN_PRINT)) {
+		//printStatement();
+	//}
+}
+
 bool compile(const char* source, Chunk* chunk) {
 	initScanner(source);
 	compilingChunk = chunk;
@@ -266,8 +278,9 @@ bool compile(const char* source, Chunk* chunk) {
 	parser.panicMode = false;
 
 	advance();
-	expression();
-	consume(TOKEN_EOF, "Expect end of expression.");
+	/*while (!match(TOKEN_EOF)) {
+		declaration();
+	}*/
 	endCompiler();
 
 	return !parser.hadError;	
